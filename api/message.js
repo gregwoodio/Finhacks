7205995e001424f488ddca684eda78167678ec66
weirdvector <weirdvector@gmail.com>
2016-11-19 16:53:10 -0500

    Messaging works

--- api/message.js
+++ api/message.js
@@ -5,60 +5,28 @@ var request = require('request');
 module.exports = function(app, models) {
   app.post('/message', function(req, res) {
 
-    // if (req.body.deviceId) {
-
-    var response = undefined;
+    console.log(req.body.deviceId);
+    console.log(req.body.message);
 
     request({
-      // url: 'http://159.203.8.134:3000/login',
       url: 'https://fcm.googleapis.com/fcm/send',
       method: 'post',
       headers: {
-        Authorization: 'key=AIzaSyAokS3NEjFMIWTtYXUZWMjmGog0vmLFulE',
-        Content: 'application/json'
+        "Authorization": 'key=AAAAeIQERUc:APA91bGEKsiM-04ISYYVfVv0wIgUIkmq50C4aGmxRclhX1oqEmbla-H8DBtWQON1LcKMJnpShjNohyXRTIicsOHRqzDuTVFQ6TuMluDJIT46Thl6GyO_2KKRDBFR_b--zot9mdxKoMd2TjMmS3R-Mp64BYaP66ZjAA',
+        "Content-Type": 'application/json'
       },
-      // json: {
-      //   email: 'greg@email.com',
-      //   password:'password'
-      // }
       json: {
-        data: { "collapse_key": "score_update",
-          "time_to_live": 108,
-          "data": {
-            "score": "4x8",
-            "time": "15:16.2342"
-          },
-          "to" : req.body.deviceId
+        'to': req.body.deviceId,
+        'notification':   {
+          'body': req.body.message
         }
       }
     }, function(err, response, body) {
-      console.log(response);
+      // console.log(response);
       res.json({
-        success: true,
-        data: response
+        response
       });
     });
-    
-    // } else {
-    //   res.json({
-    //     success: false,
-    //     message: 'Device ID was not specified.'
-    //   });
-    // }
 
   });
 };
-
-
-//     request.post('', { 
-//       json: {
-//         to: req.body.deviceId,
-//         data: "test message"
-//       } 
-//     },
-//         function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//           console.log(body);
-//         }
-//     }
-// );
