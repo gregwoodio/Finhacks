2bc713710b634612e59a9a1aae1e559236299d5f
weirdvector <weirdvector@gmail.com>
2016-11-19 16:59:25 -0500

    fixes messaging

--- api/message.js
+++ api/message.js
@@ -5,17 +5,28 @@ var request = require('request');
 module.exports = function(app, models) {
   app.post('/message', function(req, res) {
 
+    console.log(req.body.deviceId);
+    console.log(req.body.message);
 
-    request.post('http://www.yoursite.com/formpage', { 
+    request({
+      url: 'https://fcm.googleapis.com/fcm/send',
+      method: 'post',
+      headers: {
+        "Authorization": 'key=AAAAeIQERUc:APA91bGEKsiM-04ISYYVfVv0wIgUIkmq50C4aGmxRclhX1oqEmbla-H8DBtWQON1LcKMJnpShjNohyXRTIicsOHRqzDuTVFQ6TuMluDJIT46Thl6GyO_2KKRDBFR_b--zot9mdxKoMd2TjMmS3R-Mp64BYaP66ZjAA',
+        "Content-Type": 'application/json'
+      },
       json: {
-        key: 'value' 
-      } 
-    },
-        function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          console.log(body);
+        'to': req.body.deviceId,
+        'notification':   {
+          'body': req.body.message
         }
-    }
-);
+      }
+    }, function(err, response, body) {
+      // console.log(response);
+      res.json({
+        response
+      });
+    });
+
   });
 };
