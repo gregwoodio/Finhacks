a98ee7591d99c984a409bdd116712e3fdb88add9
JLIVE101 <jwilliams199055@gmail.com>
2016-11-20 04:27:55 -0500

    modified uploads route to accept files from android

--- api/uploads.js
+++ api/uploads.js
@@ -2,6 +2,7 @@ var multiparty           = require('connect-multiparty');
 var multiPartyMiddleware = multiparty();
 var path                 = require('path');
 var fs                   = require('fs');
+var mw                   = require('../middleware');
 
 PROFILE_UPLOADS_PATH     = path.join(__dirname,"..","uploads");
 ACCEPTED_FILE_EXTENSIONS = [".jpg",".jpeg",".png",".bmp"];
@@ -9,28 +10,42 @@ ACCEPTED_FILE_EXTENSIONS = [".jpg",".jpeg",".png",".bmp"];
 module.exports = function(app, models) {
 
 
-  app.post('/uploadProfilePicture', multiPartyMiddleware,  function (req, res) {
+  app.post('/uploadProfilePicture', [multiPartyMiddleware, mw.verifyToken],  function (req, res) {
 
-      var file = req.files.file;
-      var fileExtension = path.extname(file.originalFilename);
+
+      //base 64 encoded image file
+      var file = req.body.image;
+
 
       if(file) {
 
+        var imageBuffer = decodeBase64Image(file);
+
         //generate new file name using the magnetid of a users profile
-        var newFileName = req.body.magnetid + fileExtension;
-        //generate new path to file name
-        var newPath = path.join(PROFILE_UPLOADS_PATH, newFileName);
+        var newFileName = req.body.magnetid + ".jpg";
 
-        fs.readFile(file.path, function (err, data) {
+        fs.writeFile(newFileName, imageBuffer.data, function (err) {
+          if(err)
+            return res.status(500).send("error " + err.message);
 
-          fs.writeFile(newPath, data, function (err) {
-            if(err)
-              return res.status(500).json({success: false, message: err.message});
-
-            return res.json({success: true, data: "http://localhost:3000/uploads/" + newFileName});
-          });
+          return res.send("http://159.203.8.134:3000/" + newFileName + "-success");
         });
+      } else {
+        res.send("no image sent");
       }
     });
 
+    function decodeBase64Image(dataString) {
+      var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+          response = {};
+
+      if (matches.length !== 3) {
+          return new Error('Invalid input string');
+      }
+
+      response.type = matches[1];
+      response.data = new Buffer(matches[2], 'base64');
+
+      return response;
+  }
 };
